3ab9a41b129711783d41b5e9a4138f70c6c31b72
bikiev bayel <vetrovlena42@gmail.com>
2024-02-19 13:40:49 +0600

    dada net net

---
+++ script.js
@@ -0,0 +1,37 @@
+
+console.log('My name is' + ' Bayel');
+
+
+console.log("Total cost" + ' $' + (5 + 3));
+
+
+console.log(`Total cost $${8}`);
+
+
+alert(`Total cost $${8}`);
+
+
+console.log( 'Total cost'+' $'+(599+295)/100);
+
+
+console.log(`Total cost $${(599+295)/100}`);
+
+
+alert(`Total cost $${(599+295)/100} Thank you, come again`)
+
+let a = 'items(4)  $';
+let myAge = 2 * 2095;
+let maName = 2 * 799;
+let division = 100;
+let total =myAge + maName;
+let conflikt =  total / division
+console.log(a,conflikt);
+
+
+console.log('Shipping'+' & '+'handling'+' $'+(499+499)/100);
+
+console.log('Total '+' before'+' tax'+' $'+((2*2095)+(2*799)+499+499)/100);
+
+console.log( `Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 499 + 499) * 0.1) / 100}`
+);
+
